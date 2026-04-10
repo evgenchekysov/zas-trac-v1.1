d0a81df7f6707fbc33eb3eeec516923d76a8d0e5
evgenchekysov <evgenchekysov@gmail.com>
2026-04-10 13:30:09 +0300

    Add custom error classes for ticket management

--- backend/core/errors.py
+++ backend/core/errors.py
@@ -1,1 +1,10 @@
-errors.py
+class TicketError(Exception):
+    """Base domain error for tickets"""
+
+
+class InvalidStatusTransition(TicketError):
+    pass
+
+
+class ParticipantError(TicketError):
+    pass
