7986ef1dc6b8a97e07276accb003f2dc900e3330
evgenchekysov <evgenchekysov@gmail.com>
2026-04-17 12:56:55 +0300

    Update asset_repo.py

--- backend/db/asset_repo.py
+++ backend/db/asset_repo.py
@@ -1,1 +1,16 @@
+from uuid import UUID
+from typing import Optional
 
+
+class AssetRepo:
+    async def get(self, asset_id: UUID):
+        """
+        Возвращает объект оборудования или None.
+        """
+        raise NotImplementedError
+
+    async def find_by_code(self, code: str):
+        """
+        Поиск оборудования по коду / QR.
+        """
+        raise NotImplementedError
