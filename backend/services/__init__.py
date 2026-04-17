24aa87e4f33c633a79d2bc2f021ab79a2e8af54f
evgenchekysov <evgenchekysov@gmail.com>
2026-04-17 14:57:01 +0300

    Update __init__.py

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,8 +1,15 @@
 from services.session_service import session_service
 from services.ticket_service import ticket_service
 from services.session_workflow import SessionWorkflow
+from services.ticket_workflow import TicketWorkflow
+
 
 session_workflow = SessionWorkflow(
     session_service=session_service,
+    ticket_service=ticket_service,
+)
+
+
+ticket_workflow = TicketWorkflow(
     ticket_service=ticket_service,
 )
